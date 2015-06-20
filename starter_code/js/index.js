7d072eed9b19967a48039b302c643ecf4d695660
sarahong2012 <sarahong2012@gmail.com>
2015-06-19 21:06:50 -0400

    syncing my new repository

---
+++ starter_code/js/index.js
@@ -0,0 +1,36 @@
+console.log ("hello")
+
+/*
+Listen for form submit.
+Form submit occurs when submit-btn is clicked on. 
+Obtain value of city-type. 
+Assign to var city. 
+If city equals NYC change background to nyc.jpg by adding .nyc to body.
+Else If city equals LA change background to la.jpg by adding .la to body.
+else If city equals SF change background to sf.jpg by adding .sf to body.
+else If city equals Austin change background to austin.jpg by adding .sydney to body. etc.
+*/
+
+$( document ).ready(function() {
+    console.log("lets go");
+  });
+
+$("form").on("submit", function(event){
+	event.preventDefault();
+	var city = $("#city-type").val();
+	if(city == "nyc" || city == "NYC" || city == "New York" || city == "new york"){
+		$("body").removeClass().addClass("nyc");
+	}
+	else if(city == "LA" || city == "la" || city == "Los Angeles" || city == "los angeles"){
+		$("body").removeClass().addClass("la");
+	}
+	else if(city == "Austin" || city == "austin"){
+		$("body").removeClass().addClass("austin");
+	}
+	else if(city == "SF" || city == "sf" || city == "san fran" || city == "san francisco" || city == "San Francisco"){
+		$("body").removeClass().addClass("sf");
+	}
+	else if(city == "Sydney" || city == "sydney"){
+		$("body").removeClass().addClass("sydney");
+	}
+})
